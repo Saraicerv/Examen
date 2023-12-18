2d3281340f9f732960b10ee493ce4796f354151c
Saraicerv <153331239+Saraicerv@users.noreply.github.com>
2023-12-18 00:27:10 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+
+<head>
+
+<title>Informacion Personal</title>
+
+</head>
+
+<body>
+
+<table>
+
+<tr>
+
+<td bgcolor="#800080">Sarai </td> <!--  -->
+<td bgcolor="#00FF00">Cervantes</td> <!-- -->
+<td bgcolor="#8A2BE2">21090986</td> <!--  -->
+<td bgcolor="#CD5C5C">Programacion web</td> <!--  -->
+<td bgcolor="#F1C40F">Examen</td> <!--   -->
+
+</tr>
+
+</table>
+
+</body>
+
+</html>
